a2fcfcf3813f13d5e54af8f4faab887b00f1f792
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-25 17:57:47 +0200

    add 4

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 // costruzione da 1 a 100
 const boxes = document.querySelector ('.boxes');
 
+
 for (let i = 1; i <= 100; i++) {
-    boxes.innerHTML += `<div class="box">${i}</div`
-    
-    
-    if (i % 3 ===0){
-        boxes.innerHTML += `<div class="box-3>${i}</div`
+    boxes.innerHTML += `<div class="box">${i}</div`   
+
+    if (i % 3 ===0) {
+        boxes.innerHTML += `<div class="box-3">${i}</div`
     }
+
 }
+
 
 
 
