6e57518ac078555b4453782cec8b3e45ba561276
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-25 14:41:25 +0200

    add costruzione da 1 a 100

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log('ciao');
+// costruzione da 1 a 100
+const boxes = document.querySelector ('.boxes');
+
+for (let i = 1; i <= 100; i++) {
+    boxes.innerHTML += `<div class="box box-${i}">${i}</div>`
+}
