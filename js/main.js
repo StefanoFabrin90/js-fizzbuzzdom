546b85174c3b570738b79ceb22761f554d374420
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-26 14:43:55 +0200

    add correzione

--- js/main.js
+++ js/main.js
@@ -3,11 +3,21 @@ const boxes = document.querySelector ('.boxes');
 
 
 for (let i = 1; i <= 100; i++) {
-    boxes.innerHTML += `<div class="box">${i}</div`   
+     
+    
+    let element = '';
 
-    if (i % 3 ===0) {
-        boxes.innerHTML += `<div class="box-3">${i}</div`
+    if ( (i % 3 === 0) && (i % 5 ===0) ) {
+        element = `<div class="box box-fizzbuzz">fizzbuzz</div`
+    } else if (i % 3 ===0) {
+        element = `<div class="box box-fizz">fizz</div`
+    } else if (i % 5 ===0) {
+        element = `<div class="box box-buzz">buzz</div`
+    } else {
+        element = `<div class="box">${i}</div`
     }
+    
+    boxes.innerHTML += element;
 
 }
 
